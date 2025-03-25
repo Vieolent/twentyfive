7bd2f84e29ac0ac57d6e3f16dca7e38552dab592
Vieolent <miranda.l.bache@gmail.com>
2025-03-25 20:47:23 +0100

    2 sidor down

--- js/main.js
+++ js/main.js
@@ -3,17 +3,19 @@ document.querySelector(".menu-button").addEventListener("click", showMenu)
 
 
 function showMenu(){
+    console.log("click menu");
     let menu = document.querySelector("nav.menu");
     menu.classList.toggle("show");
-
+    
     let layer = document.querySelector(".fade-layer");
     layer.classList.toggle("visible");
 }
 
-function showDropdown(){
-    let dropDown = document.querySelector("div.drop-button");
-    menu.classList.toggle("show");
-
-    let layer = document.querySelector(".fade-layer");
-    layer.classList.toggle("visible");
-}
+    document.querySelector(".drop-button").addEventListener("click", showDropdown)
+    
+    function showDropdown(){
+        console.log("click dropdown");
+        let aside = document.querySelector("aside");
+        aside.classList.toggle("appear");
+        
+    }    
